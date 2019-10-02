e43ecceb1b87a6cdc40f3e5a5df9ff8aa89cf315
Andrew <pi_r_round@hotmail.com>
2019-10-02 17:36:44 -0500

    Completed Object Oriented Shopping Cart Lab.

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,47 @@
 class ShoppingCart:
     # write your code here
     def __init__(self, emp_discount=None):
-      pass
+      self.total = 0
+      self.employee_discount = emp_discount
+      self.items = []
     def add_item(self, name, price, quantity=1):
-       pass
+       self.items.append([name, price, quantity])
+       self.total += price * quantity if quantity > 0 else price
+       return f'New cart total is ${self.total}.'
     def mean_item_price(self):
-       pass
-
+        prices = []
+        for n, p, q in self.items:
+            count = q
+            while count >= 1:
+                count -= 1
+                prices.append(p)
+        return round((sum(prices)/len(prices)) - .005, 2)
+        
     def median_item_price(self):
-        pass
+        prices = []
+        for n, p, q in self.items:
+            count = q
+            while count >= 1:
+                count -= 1
+                prices.append(p)
+        if len(prices)%2==1:
+            return sorted(prices)[(len(prices)//2)]
+        else:
+            return round((sorted(prices)[len(prices)//2] +
+                   sorted(prices)[(len(prices)//2) + 1]) / 2, 2)
 
     def apply_discount(self):
-       pass
+        if self.employee_discount:
+            return f'''Discounted total is ${round(self.total * ((100 - self.employee_discount) / 100), 2)}.'''
+        return 'Sorry, there is no discount to apply to your cart. :('
 
     def void_last_item(self):
-        pass
+        if len(self.items) >= 1:
+            if self.items[-1][2] > 1:
+                self.items[-1][2] -= 1
+                self.total -= self.items[-1][1]
+            else:
+                self.total -= self.items[-1][1]
+                self.items.remove[-1]
+        else:
+            return "There are no items in your cart!"
